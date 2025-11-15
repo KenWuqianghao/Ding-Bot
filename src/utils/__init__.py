2d2e0ff54724840f81b455019994af16628da42b
Ken <wooqianghao@gmail.com>
2025-11-15 16:36:11 -0500

    Added manager

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,5 +1,5 @@
 from .board_utils import board_to_fen, fen_to_board, is_game_over
 from .time_management import allocate_time, allocate_time_aggressive, is_critical_position, estimate_move_number
+from .decorator import chess_manager, GameContext
 
-__all__ = ['board_to_fen', 'fen_to_board', 'is_game_over', 'allocate_time', 'allocate_time_aggressive', 'is_critical_position', 'estimate_move_number']
-
+__all__ = ['board_to_fen', 'fen_to_board', 'is_game_over', 'allocate_time', 'allocate_time_aggressive', 'is_critical_position', 'estimate_move_number', 'chess_manager', 'GameContext']
